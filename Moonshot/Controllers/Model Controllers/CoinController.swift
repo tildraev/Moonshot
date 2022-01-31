ce19e5a10d8b8f82131fbf4c632e6d40147f7bc7
Arian Mohajer <amohajer45@gmail.com>
2022-01-31 13:30:11 -0700

    theoretically completed the networking section of this PPP

--- Moonshot/Controllers/Model Controllers/CoinController.swift
+++ Moonshot/Controllers/Model Controllers/CoinController.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+class CoinController {
+    
+    static var coins: [Coin] = []
+    private static var baseURLString: String = "https://api.coingecko.com/api/v3"
+    private static var keyCoinsComponent: String = "/coins"
+    private static var keyListsComponent: String = "/list"
+    
+    
+    static func fetchCoins(completion: @escaping (Bool) -> Void) {
+        guard let baseURL = URL(string: baseURLString) else { return completion(false)}
+        var coinsURL = baseURL.appendingPathComponent(keyCoinsComponent)
+        var finalURL = coinsURL.appendingPathComponent(keyCoinsComponent)
+        print(finalURL)
+        
+        URLSession.shared.dataTask(with: finalURL) { coinData, _, error	 in
+            if let error = error {
+                print("There was an error: \(error.localizedDescription)")
+                completion(false)
+            } else {
+                guard let data = coinData else { completion(false); return }
+                do {
+                    if let topLevelArrayOfCoinDictionaries = try JSONSerialization.jsonObject(with: data, options: []) as? [[String:String]] {
+                        for coinDictionary in topLevelArrayOfCoinDictionaries {
+                            if let id = coinDictionary["id"],
+                               let symbol = coinDictionary["symbol"],
+                               let name = coinDictionary["name"] {
+                                var parsedCoin = Coin(id: id, symbol: symbol, name: name)
+                                coins.append(parsedCoin)
+                            }
+                        }
+                    }
+                    completion(true)
+                } catch {
+                    print("Error in do/try/catch: \(error.localizedDescription)")
+                    completion(false)
+                }
+            }
+        }.resume()
+        
+    }
+}
--- Moonshot/Models/Coin.swift
+++ Moonshot/Models/Coin.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class Coin {
+    var id: String
+    var symbol: String
+    var name: String
+    
+    init(id: String, symbol: String, name: String) {
+        self.id = id
+        self.symbol = symbol
+        self.name = name
+    }
+}
